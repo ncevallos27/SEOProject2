7e8901d966786e4934b82d4a909acbb086b477f2
EdsonPetry <edsonlbpetry@gmail.com>
2025-07-03 01:17:48 -0400

    feat: prototype function calling feature of google-genai api as well as ensure google calendar api works by printing 15 events from logged in users calendar. Changed load_dotenv(Path(.env)) to just load_dotenv()

--- main.py
+++ main.py
@@ -1,41 +1,134 @@
-from pathlib import Path
+import datetime
+import os.path
 from dotenv import load_dotenv
-import os
-import pandas as pd
 
-from modules.GoogleAPI import GoogleAPI
-from modules.Database import Database
-from modules.Agent import Agent
-from modules.GoogleCalAPI import GoogleCalAPI
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google import genai
+from google.genai import types
 
 SQLPATH = "sqlite:///events.db"
+SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 
-# TODO: add Doc String
+def schedule_meeting(attendees: list, date: str, time: str, topic: str):
+    """
+    Schedules a meeting with specified attendees at a given time and date.
+
+    Parameters:
+        --attendees: List of people attending the meeting.
+        --date: Date of the meeting (e.g., '2024-07-29)
+        --time: Time of the meeting (e.g., '15:00')
+        --topic: The subject or topic of the meeting.
+    """
+
+    return f"Scheduled meeting for {attendees} at {date} {time} to discuss {topic}"
+
+schedule_meeting_function = {
+    "name": "schedule_meeting",
+    "description": "Schedules a meeting with specified attendees at a given time and date.",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "attendees": {
+                "type": "array",
+                "items": {"type": "string"},
+                "description": "List of people attending the meeting.",
+            },
+            "date": {
+                "type": "string",
+                "description": "Date of the meeting (e.g., '2024-07-29')",
+            },
+            "time": {
+                "type": "string",
+                "description": "Time of the meeting (e.g., '15:00')",
+            },
+            "topic": {
+                "type": "string",
+                "description": "The subject or topic of the meeting.",
+            },
+        },
+        "required": ["attendees", "date", "time", "topic"],
+    },
+}
+
+
 def main():
-    load_dotenv(Path(".env"))
+    """
+    TODO!: Write docstring for main()
+    """
+    load_dotenv() # reads .env by default (no need for Path module)
+    creds = None
 
-    gcapi = GoogleCalAPI(pathCred=Path("credentials.json").resolve(), pathToken=Path("token.json").resolve())
-    gapi = GoogleAPI(apiKey=os.getenv('GENAI_KEY'))
-    dbase = Database(path=SQLPATH)
-    agent = Agent(gapi, dbase, gcapi)
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+        
+    try:
+        service = build("calendar", "v3", credentials=creds)
 
-    print("\nPersonal Assistant CLI App\n")
-    print("Type q to quit or h for help")
-    while True:
-        userInput = input("[APP NAME] Enter what you would like me to do: ")
-        match userInput.lower():
-            case 'q':
-                break
-            case 'h':
-                print("\nHelp Screen")
-                continue
-            case _:
-                answer = agent.getAnswer(userInput)
-                
-                continue
+        # Calling the Calendar API
+        now = datetime.datetime.now(tz=datetime.timezone.utc).isoformat()
+        print("Getting the upcoming 15 events")
+        events_result = (
+            service.events()
+            .list(
+                calendarId="primary",
+                timeMin=now,
+                maxResults=15,
+                singleEvents=True,
+                orderBy="startTime",
+            )
+            .execute()
+        )
+        events = events_result.get("items", [])
 
+        if not events:
+            print("No upcoming events found.")
+            return
+        
+        for event in events:
+            start = event["start"].get("dateTime", event["start"].get("date"))
+            print(start, event["summary"])
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+    
+    # Config client and tools
+    client = genai.Client()
+    tools = types.Tool(function_declarations=[schedule_meeting_function])
+    config = types.GenerateContentConfig(tools=[tools])
 
-    print("\nGoodBye")
+    # Send request with function declarations
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=input("Enter prompt: "),
+        config=config,
+    )
+
+    # Check for function call
+    if response.candidates[0].content.parts[0].function_call:
+        function_call = response.candidates[0].content.parts[0].function_call
+        print(f"Function to call: {function_call.name}")
+        print(f"Arguments: {function_call.args}")
+        result = schedule_meeting(**function_call.args)
+        print(f"result: {result}")
+    else:
+        print("No funciton call found in the response.")
+        print(response.text)
+    
 
 
 if __name__ == "__main__":
